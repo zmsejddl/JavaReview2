21cca6f558619051ae676a2745cd0425d09e5f81
Jung Young Hun <hujung0268@gamil.com>
2024-07-23 16:39:32 +0900

    참조 자료형 - String

---
+++ 자바 복습/src/SECTION02_변수와_자료형/참조_자료형_04/예제2_20.java
@@ -0,0 +1,13 @@
+package SECTION02_변수와_자료형.참조_자료형_04;
+
+public class 예제2_20 {
+	public static void main(String[] args) {
+		//참조 자료형
+		
+		String hi = "안녕하세요,";
+		String niceToMeetYou = "만나서 반갑습니다.";
+		
+		System.out.println(hi);
+		System.out.println(niceToMeetYou);
+	}
+}
