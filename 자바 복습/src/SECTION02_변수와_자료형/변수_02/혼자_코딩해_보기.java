ab2a1b06c3ad973b3f0b8e7e67f4498642083d30
Jung Young Hun <hujung0268@gamil.com>
2024-07-23 13:25:51 +0900

    혼자 코딩해 보기

---
+++ 자바 복습/src/SECTION02_변수와_자료형/변수_02/혼자_코딩해_보기.java
@@ -0,0 +1,12 @@
+package SECTION02_변수와_자료형.변수_02;
+
+public class 혼자_코딩해_보기 {
+	public static void main(String[] args) {
+		//변수 이름을 나이와 관련된 이름으로 규칙에 따라 지어보고, 본인의 나이를 대입하여 출력해 보세요.
+		
+		int age;
+		age = 25;
+		
+		System.out.println(age);
+	}
+}
