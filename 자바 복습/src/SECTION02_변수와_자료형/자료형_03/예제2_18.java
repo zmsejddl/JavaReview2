c5532b4e5a8430cf860dccc169c405ba2783d99c
Jung Young Hun <hujung0268@gamil.com>
2024-07-23 13:44:52 +0900

    논리형 boolean

---
+++ 자바 복습/src/SECTION02_변수와_자료형/자료형_03/예제2_18.java
@@ -0,0 +1,10 @@
+package SECTION02_변수와_자료형.자료형_03;
+
+public class 예제2_18 {
+	public static void main(String[] args) {
+		//논리형 1
+		
+		boolean isStudent = true;
+		System.out.println(isStudent);	//출력 결과: true
+	}
+}
---
+++ 자바 복습/src/SECTION02_변수와_자료형/자료형_03/예제2_19.java
@@ -0,0 +1,15 @@
+package SECTION02_변수와_자료형.자료형_03;
+
+public class 예제2_19 {
+	public static void main(String[] args) {
+		//논리형 2
+		
+		boolean isStudent = true;
+		
+		if(isStudent) {	//isStudent가 true이면,
+			System.out.println("저는 학생입니다.");
+		} else {	//isStudent가 false이면,
+			System.out.println("저는 학생이 아닙니다.");
+		}
+	}
+}
