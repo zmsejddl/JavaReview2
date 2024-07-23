848a26cab682fcac16173a5b1ff002ae3b1040f1
Jung Young Hun <hujung0268@gamil.com>
2024-07-23 13:34:53 +0900

    실수형 float, double

---
+++ 자바 복습/src/SECTION02_변수와_자료형/자료형_03/예제2_13.java
@@ -0,0 +1,14 @@
+package SECTION02_변수와_자료형.자료형_03;
+
+public class 예제2_13 {
+	public static void main(String[] args) {
+		//실수형 1
+		
+		double marathon = 42.195;
+		//float halfMarathon = 21.0975; 구문 오류 발생
+		float halfMarathon = 21.0975f;
+		
+		System.out.println("마라톤은 " + marathon + "km를 달립니다.");
+		System.out.println("하프 마라톤은 " + halfMarathon + "km를 달립니다.");
+	}
+}
---
+++ 자바 복습/src/SECTION02_변수와_자료형/자료형_03/예제2_14.java
@@ -0,0 +1,13 @@
+package SECTION02_변수와_자료형.자료형_03;
+
+public class 예제2_14 {
+	public static void main(String[] args) {
+		//실수형 2 <정밀도 확인하기>
+		
+		double pieDouble = 3.1415922653589793;
+		float pieFloat = 3.1415922653589793f;
+		
+		System.out.println("double: " + pieDouble);
+		System.out.println("float: " + pieFloat);
+	}
+}
