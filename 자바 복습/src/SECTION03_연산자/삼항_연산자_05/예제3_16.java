6983f412332ddb2f04cf1587bb8e7bd0488ef898
Jung Young Hun <hujung0268@gamil.com>
2024-07-24 12:21:52 +0900

    삼항 연산자

---
+++ 자바 복습/src/SECTION03_연산자/삼항_연산자_05/예제3_16.java
@@ -0,0 +1,10 @@
+package SECTION03_연산자.삼항_연산자_05;
+
+public class 예제3_16 {
+	public static void main(String[] args) {
+		//삼항 연산자 1
+		
+		int num = (7 > 1) ? 1 : 2;
+		System.out.println(num);
+	}
+}
---
+++ 자바 복습/src/SECTION03_연산자/삼항_연산자_05/예제3_17.java
@@ -0,0 +1,15 @@
+package SECTION03_연산자.삼항_연산자_05;
+
+public class 예제3_17 {
+	public static void main(String[] args) {
+		// 삼항 연산자 2
+
+		int num = 0;
+		if (7 > 1) {
+			num = 1;
+		} else {
+			num = 2;
+		}
+		System.out.println(num);
+	}
+}
