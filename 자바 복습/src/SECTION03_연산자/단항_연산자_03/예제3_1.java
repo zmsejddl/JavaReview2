d0f6d27a62c2c2fdfc46f23e9f94100e6dd73c35
Jung Young Hun <hujung0268@gamil.com>
2024-07-24 11:52:39 +0900

    부호 연산자 +, -

---
+++ 자바 복습/src/SECTION03_연산자/단항_연산자_03/예제3_1.java
@@ -0,0 +1,14 @@
+package SECTION03_연산자.단항_연산자_03;
+
+public class 예제3_1 {
+	public static void main(String[] args) {
+		//부호 연산자
+		
+		int x = 100;
+		int resultPlus = +x;	//+ 부호
+		int resultMinum = -x;	//- 부호
+		
+		System.out.println(resultPlus);
+		System.out.println(resultMinum);
+	}
+}
---
+++ 자바 복습/src/SECTION03_연산자/단항_연산자_03/예제3_2.java
@@ -0,0 +1,13 @@
+package SECTION03_연산자.단항_연산자_03;
+
+public class 예제3_2 {
+	public static void main(String[] args) {
+		// 부호 연산자 <실수>
+
+		double d = 1.11;
+		double result = -d;
+
+		System.out.println(-d);
+		System.out.println(result);
+	}
+}
