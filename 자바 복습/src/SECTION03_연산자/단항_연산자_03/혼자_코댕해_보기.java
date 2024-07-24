f98b5b3084fd52d1c5c00f13c01478b715946ec4
Jung Young Hun <hujung0268@gamil.com>
2024-07-24 12:02:13 +0900

    혼자 코딩해 보기

---
+++ 자바 복습/src/SECTION03_연산자/단항_연산자_03/혼자_코댕해_보기.java
@@ -0,0 +1,12 @@
+package SECTION03_연산자.단항_연산자_03;
+
+public class 혼자_코댕해_보기 {
+	public static void main(String[] args) {
+		
+		int num1 = 100;
+		
+		System.out.println(num1++);
+		System.out.println(++num1);
+		System.out.println(++num1);
+	}
+}
