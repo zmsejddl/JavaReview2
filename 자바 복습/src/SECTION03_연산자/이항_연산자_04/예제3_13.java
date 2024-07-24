08ebfacdd376501aaec996b33a157c1398dbbb7b
Jung Young Hun <hujung0268@gamil.com>
2024-07-24 12:14:59 +0900

    관계 연산자 <, <=, >=, ==, !=

---
+++ 자바 복습/src/SECTION03_연산자/이항_연산자_04/예제3_13.java
@@ -0,0 +1,15 @@
+package SECTION03_연산자.이항_연산자_04;
+
+public class 예제3_13 {
+	public static void main(String[] args) {
+		// 관계 연산자 1
+
+		int a = 10;
+		int b = 20;
+
+		System.out.println(a > b);
+		System.out.println(a <= b);
+		System.out.println(a == b);
+		System.out.println(a != b);
+	}
+}
---
+++ 자바 복습/src/SECTION03_연산자/이항_연산자_04/예제3_14.java
@@ -0,0 +1,14 @@
+package SECTION03_연산자.이항_연산자_04;
+
+public class 예제3_14 {
+	public static void main(String[] args) {
+		// 관계 연산자 2
+
+		int a = 10;
+		int b = 11;
+
+		System.out.println(a <= b); // 코드 1
+		System.out.println(a == b); // 코드 2
+		System.out.println(a != --b); // 코드 3
+	}
+}
