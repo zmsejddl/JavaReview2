757981507008813772b2040b312490720517bc42
Jung Young Hun <hujung0268@gamil.com>
2024-07-24 17:20:25 +0900

    혼자 코딩해 보기

---
+++ 자바 복습/src/SECTION04_제어문_1/if문_03/혼자_코딩해_보기.java
@@ -0,0 +1,13 @@
+package SECTION04_제어문_1.if문_03;
+
+public class 혼자_코딩해_보기 {
+	public static void main(String[] args) {
+		// int형 변수 num에 정수 10을 저장하고, 숫자가 5 이상이면 "num은 5보다 큽니다."라고 출력해 보세요.
+
+		int num = 10;
+
+		if (num > 5) {
+			System.out.println("num은 5보다 큽니다.");
+		}
+	}
+}
