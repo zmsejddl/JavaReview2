6783032f0f721ba566aa72115843cc460d859a26
Jung Young Hun <hujung0268@gamil.com>
2024-07-24 12:00:49 +0900

    논리 부정 연산자 !

---
+++ 자바 복습/src/SECTION03_연산자/단항_연산자_03/예제3_8.java
@@ -0,0 +1,11 @@
+package SECTION03_연산자.단항_연산자_03;
+
+public class 예제3_8 {
+	public static void main(String[] args) {
+		// 논리 부정 연산자
+		
+		boolean isHuman = false;
+		System.out.println(!isHuman);
+		System.out.println(isHuman);
+	}
+}
