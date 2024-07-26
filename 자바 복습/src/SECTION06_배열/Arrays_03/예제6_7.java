0ae5a9e8a511d71c6c7da34186bde23ab82b6fd9
Jung Young Hun <hujung0268@gamil.com>
2024-07-26 13:53:46 +0900

    배열의 출력

---
+++ 자바 복습/src/SECTION06_배열/Arrays_03/예제6_7.java
@@ -0,0 +1,19 @@
+package SECTION06_배열.Arrays_03;
+
+import java.util.Arrays;
+
+public class 예제6_7 {
+	public static void main(String[] args) {
+		// Arrays.toString()
+
+		int[] arr = { 1, 6, 2, 3, 10, 7, 4, 5, 8, 9 };
+
+		System.out.println(Arrays.toString(arr));
+		
+		/*
+		 * 해설
+		 * 03: Arrays 기능을 사용하기 위해 선언합니다.
+		 * 11: Arrays.toString(배열)은 배열을 하나의 문자열로 출력해 줍니다.
+		 */
+	}
+}
