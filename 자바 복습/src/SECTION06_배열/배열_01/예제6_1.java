0bd36c11317f47bc453a440e90d17a59b3478185
Jung Young Hun <hujung0268@gamil.com>
2024-07-26 13:17:38 +0900

    배열의 생성과 선언

---
+++ 자바 복습/src/SECTION06_배열/배열_01/예제6_1.java
@@ -0,0 +1,12 @@
+package SECTION06_배열.배열_01;
+
+public class 예제6_1 {
+	public static void main(String[] args) {
+		// 배열의 선언과 생성
+
+		int[] arr = new int[4];
+
+		// 배열 변수(참조형 변수)를 출력
+		System.out.println(arr);
+	}
+}
