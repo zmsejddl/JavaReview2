877c5bf2a5f88534e6afbe3ddcd95b247e8d829c
Jung Young Hun <hujung0268@gamil.com>
2024-07-29 13:43:59 +0900

    getter와 setter 메서드

---
+++ 자바 복습/src/SECTION08_메서드/getter와_setter메서드_03/예제8_20.java
@@ -0,0 +1,7 @@
+package SECTION08_메서드.getter와_setter메서드_03;
+
+//마음대로 접근 가능한 필드
+
+public class 예제8_20 {
+	int age;
+}
---
+++ 자바 복습/src/SECTION08_메서드/getter와_setter메서드_03/예제8_21.java
@@ -0,0 +1,11 @@
+package SECTION08_메서드.getter와_setter메서드_03;
+
+//마음대로 수정 가능한 필드
+
+public class 예제8_21 {
+	public static void main(String[] args) {
+		예제8_20 kim = new 예제8_20();
+		kim.age = -30;
+		System.out.println("kim의 나이는 " + kim.age + "세입니다.");
+	}
+}
