e7d434facdfe2cb20cca4f9310133358debed5f2
Jung Young Hun <hujung0268@gamil.com>
2024-07-31 13:05:09 +0900

    상속이란?

---
+++ 자바 복습/src/SECTION10_상속/상속_01/예제10_1.java
@@ -0,0 +1,12 @@
+package SECTION10_상속.상속_01;
+
+//상속
+
+public class 예제10_1 { // 클래스 선언
+	String title;
+	int price;
+
+	void info() {
+		System.out.println("책의 제목은 " + title + "이고, 가격은 " + price + "원입니다.");
+	}
+}
---
+++ 자바 복습/src/SECTION10_상속/상속_01/예제10_3.java
@@ -0,0 +1,12 @@
+package SECTION10_상속.상속_01;
+
+//상속
+
+public class 예제10_3 { // 클래스 선언
+	public static void main(String[] args) {
+		예제10_2 comicBook = new 예제10_2();
+		comicBook.title = "포켓몬";
+		comicBook.price = 4500;
+		comicBook.info();
+	}
+}
